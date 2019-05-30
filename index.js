33ce086a60a93fc9cf731685a6caa114f06ace4f
Yahia Elhag <yahiae6643@hstat.org>
2019-05-30 12:58:09 +0000

    organize cover

--- index.js
+++ index.js
@@ -4,12 +4,56 @@ $(function() {
     $("#dropdown").css("display", "none")
 
 
+    var w = $(window).width()
+
+    if (w > 901) {
+        console.log(w)
+
+        $(".remove").prependTo(".bottom")
+    }
+    else {
+        console.log(w)
+        $(".remove").appendTo(".top")
+    }
+
+
+
+    $(window).resize(function() {
+
+        w = $(window).width()
+
+        if (w > 901) {
+            console.log(w)
+
+            $(".remove").prependTo(".bottom")
+        }
+        else {
+            console.log(w)
+            $(".remove").appendTo(".top")
+        }
+
+    });
+
+
+
+
+
 
     $("#button").click(function() {
         $("#dropdown").slideToggle()
+
+        // $("body").toggle(function() {$("body").css("overflow","hidden")}, function() {$("body").css("overflow","auto")})
         $(this).find("img").toggle(100)
         $("#logo").find("img").toggle()
+        $("#navbar h6").css("color", "white")
+
+
+
+
+
     })
+
+
 
 
 
